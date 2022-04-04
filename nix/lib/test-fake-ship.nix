4959b848280dc95bf86ceb314130933ca3eca8fe
Joe Bryan <joemfb@gmail.com>
2022-04-04 18:43:33 -0400

    WIP test on any platform

--- nix/lib/test-fake-ship.nix
+++ nix/lib/test-fake-ship.nix
@@ -186,6 +186,4 @@ stdenvNoCC.mkDerivation {
   # Fix 'bind: operation not permitted' when nix.useSandbox = true on darwin.
   # See https://github.com/NixOS/nix/blob/5f6840fbb49ae5b534423bd8a4360646ee93dbaf/src/libstore/build.cc#L2961
   __darwinAllowLocalNetworking = true;
-
-  meta = { platforms = [ "x86_64-linux" ]; };
 }
